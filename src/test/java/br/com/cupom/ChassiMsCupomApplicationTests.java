99547dfb9bf94372388fce17807188a37f297f87
Jefferson Diego Lopes <jefferson.lopes@e-deploy.com.br>
2020-09-02 14:01:11 -0300

    First Commit

---
+++ src/test/java/br/com/cupom/ChassiMsCupomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cupom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChassiMsCupomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
